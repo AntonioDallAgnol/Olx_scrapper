1231dec6e9940939a3a1bad95dd92f8c5f9228a7
Antônio Amadeu Dall'Agnol Rohr <103676565+AntonioDallAgnol@users.noreply.github.com>
2026-08-31 19:30:06 -0300

    Update monitor_olx.py

--- monitor_olx.py
+++ monitor_olx.py
@@ -14,7 +14,7 @@ PRECO_LIMITE = 3001.00
 DB_FILE = "anuncios_vistos.json"
 
 # Termos que você NÃO quer que apareçam no título
-PALAVRAS_BLOQUEADAS = ["vr", "portal", "psvr", "playstation portal", "vr2", "psvr2"]
+PALAVRAS_BLOQUEADAS = ["vr", "portal", "psvr", "playstation portal", "vr2", "psvr2", "metaquest", "ps4", "yotei"]
 # =================================================
 
 def carregar_historico():
